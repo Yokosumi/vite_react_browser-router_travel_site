b6c676f127ea869b8aa4068733c3338c68157dcc
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-21 09:51:22 +0100

    title changed

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./index.scss";
 
 function App() {
-  return (
-    <>
-      <h1 className="bg-red-500">hello</h1>
-    </>
-  );
+	return (
+		<>
+			<h1 className="text-4xl">Travel Agency</h1>
+			<p>testing</p>
+		</>
+	);
 }
 
 export default App;
