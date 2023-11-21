5d5b8be8b07f34e82ae9a8214857e38f9dac7b9f
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-21 10:08:37 +0100

    pages in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import "./index.scss";
+import { PageDestinations } from "./pages/PageDestination";
+import { PagePlanner } from "./pages/PagePlanner";
+import { PageTeam } from "./pages/PageTeam";
+import { PageWelcome } from "./pages/PageWelcome";
 
 function App() {
 	return (
 		<>
 			<h1 className="text-2xl mb-3">Travel Agency</h1>
-			<p>testing</p>
+			<PageWelcome />
+			<PageTeam />
+			<PageDestinations />
+			<PagePlanner />
 		</>
 	);
 }
