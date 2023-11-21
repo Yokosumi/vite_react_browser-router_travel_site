ed7529a9fdd1b40bccf01ded0670f0e50c847000
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-11-21 11:06:05 +0100

    employees data and images in

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./index.scss";
 import { AppRouter } from "./AppRouter";
+import { Nav } from "./components/Nav";
 
 function App() {
 	return (
 		<>
 			<h1 className="text-2xl mb-3">Travel Agency</h1>
+			<Nav />
 			<AppRouter />
 		</>
 	);
